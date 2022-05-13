28c1995c443926aec5b5da4f9a1f61a9d165f957
Roberto Fajo <you@example.com~robertofajo@gmail.com>
2022-05-13 07:54:42 -0300

    Con logica, pero borra leyenda

--- js/logica.js
+++ js/logica.js
@@ -15,8 +15,23 @@ function validateForm() {
       return false;
     }
     let d = document.forms["myForm"]["fcategoria"].value;
-    if (d == "") {
-      alert("Ingrese cantidad, es obligatoria");
+    if (d == ""){
+      alert("Ingrese categoria, es obligatoria");
       return false;
     }
-  }
+
+    let total=0;
+    let leyenda;
+    if(d=="Estudiante"){
+        total=200*c*0.2;
+    }else{
+        if(d=="Trainee"){
+            total=200*c*0.5;
+        }else{
+            total=200*c*0.85;
+        }
+    }
+    leyenda=document.getElementById("totalPagar");
+    document.getElementById("totalPagar").innerHTML = leyenda+"---------------------";
+
+}
